3b6d289c67ba7c7993f634f52fb0a604e245b7ba
Andrea Lee <andrealee053@gmail.com>
2019-11-16 19:49:40 -0500

    formated floating action button and added children buttons

--- src/components/list_screen/ItemCard.js
+++ src/components/list_screen/ItemCard.js
@@ -2,32 +2,44 @@ import React from 'react';
 import Button from 'react-materialize/lib/Button';
 import Icon from 'react-materialize/lib/Icon';
 
+
+
 class ItemCard extends React.Component {
     render() {
         const { item } = this.props;  
         return (
             <div className="card itemCard z-depth-1 todo-list-link light-green lighten-3">
                 <div className="card-content black-text text-darken-3">
-                    <span className="card-title">{item.description}</span>
-                    <div className="row">
-                        <div className='col s4 list_item_card_assigned_to'>
-                            Assigned To: <span className = "assigned_to">{item.assigned_to}</span>
-                        </div>
-                        <div className='col s3 list_item_card_due_date'>
-                            {item.due_date}
-                        </div>
-                        
-                        {item.completed ? 
-                            <div className='col s2'><i className="material-icons">event_available</i>&nbsp;Completed</div> : 
-                            <div className='col s2'><i className="material-icons">schedule</i>&nbsp;Pending</div>}       
 
-                        <Button
-                            floating
-                            large
-                            className="red"
-                            waves="light"
-                            icon={<Icon />}
-                        />
+                    <div className="row">
+                        <span className="col s12 card-title">{item.description}</span>
+                        
+                            <div className='col s4 list_item_card_assigned_to'>
+                                Assigned To: <span className = "assigned_to">{item.assigned_to}</span>
+                            </div>
+                            <div className='col s3 list_item_card_due_date'>
+                                {item.due_date}
+                            </div>
+                            
+                            {item.completed ? 
+                                <div className='col s2'>&nbsp;<i className="material-icons">event_available</i>&nbsp;Completed</div> : 
+                                <div className='col s2'>&nbsp;<i className="material-icons">schedule</i>&nbsp;Pending</div>}       
+
+                            <div>
+                                <Button
+                                    floating
+                                    fab={{direction: 'left'}}
+                                    className="red"
+                                    large
+                                    icon={<Icon>reorder</Icon>}
+                                    >
+                                    <Button floating icon={<Icon>keyboard_arrow_up</Icon>} className="blue" /> 
+                                    <Button floating icon={<Icon>keyboard_arrow_down</Icon>} className="green" />   
+                                    <Button floating icon={<Icon>delete</Icon>} className="yellow darken-2" />
+                                    
+                                    
+                                </Button>  
+                            </div>
 
                     </div>
 
